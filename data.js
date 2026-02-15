340a9c322762422417322c4c688943a50b65aa1f
ian <jaydeniscoolcajon@gmail.com>
2026-02-15 17:21:26 -0600

    Create data.js

---
+++ data.js
@@ -0,0 +1,21 @@
+const DATA = {
+  world_1: {
+    layer_1: {
+      coal: [1, 2],
+      iron: [1]
+    },
+    layer_2: {
+      gold: [1, 2]
+    }
+  },
+
+  world_2: {
+    layer_1: {
+      copper: [1]
+    }
+  },
+
+  world_3: {},
+  world_4: {},
+  world_5: {}
+};
